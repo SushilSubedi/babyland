8c736feddaade01a58df5b3862b4b1d09b183fb5
Sushil Subedi <sushilsubedi151@gmail.com>
2020-05-09 10:12:30 -0700

    development

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { AppBar,makeStyles,createStyles } from '@material-ui/core';
-
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
 const NavigationBar = () => {
     const classes = useStyles();
     return(
        <AppBar title="Baby Land" color='primary'>
-           hey there Navbar
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                </div>
+                <InputBase
+                    placeholder="Search…"
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'search' }}
+                />
+            </div>
        </AppBar>
     )
 }
