571da0247012ccb00038d2d46e258312af350f57
bijay subedi <subedibj3030@gmail.com>
2020-05-10 19:12:49 +0530

    second commit

---
+++ src/Component /ApplicationCard.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {Box,Typography,Button,makeStyles, createStyles,Paper} from '@material-ui/core';
+
+
+const ApplicationCard = (props) =>{
+const {name,description,imgSrc} = props;
+const classes = useStyles();
+return(
+    <Box component={Paper} className={classes.card} >
+        <div style={{marginRight:'auto',marginLeft:'auto',textAlign:'center'}}>
+            <img src={imgSrc} alt="hotel png file" className={classes.img}/>
+            <Typography>
+                <Box
+                    fontWeight= {600}
+                    fontSize={16}
+                    style={{textTransform:"capitalize",
+                    paddingTop:'10px',
+                    paddingBottom:'15px'
+                    }}
+                >
+                    {name}
+                </Box>
+            </Typography>
+            <Typography>
+                <Box
+                    fontWeight= {300}
+                    fontSize={12}
+                    style={{
+                    paddingTop:'10px',
+                    paddingBottom:'15px',
+                    color:'grey'
+                    }}
+                >
+                    {description}
+                </Box>
+            </Typography>
+            <Button className={classes.button}>
+                view
+            </Button>
+
+        </div>
+    </Box>
+)
+}
+
+const useStyles = makeStyles(theme =>
+createStyles({
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '228px',
+        height: '233px',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: '2% 1% 6% 1%',
+        marginBottom: '22%',
+        boxShadow: '0 3px 20px 0 rgba(0, 0, 0, 0.11)',
+        position:'relative',
+        border: '1px solid rgba(0,0,0,0.1)',
+        "&:hover":{
+            transform: 'translate(-5px,-20px)'
+        }
+    },
+    button:{
+        backgroundColor:'Green',
+        color:'white',
+        textTransform:'initial',
+        "&:hover":{
+            backgroundColor:'blue'
+        }
+    },
+    img:{
+        width:'32%',
+        marginTop:'16px'
+    }
+})
+)
+
+
+export default ApplicationCard;
--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -3,17 +3,17 @@ import { AppBar,makeStyles,createStyles } from '@material-ui/core';
 
 
 const NavigationBar = () => {
-    const classes = useStyles();
-    return(
-       <AppBar title="Baby Land" color='primary'>
-           hey there Navbar
-       </AppBar>
-    )
+     const classes = useStyles();
+     return(
+        <AppBar title="Baby Land" color='primary'>
+            hey there Navbar
+        </AppBar>
+     )
 }
 
-const useStyles = makeStyles(theme => 
-    createStyles({
-}))
+ const useStyles = makeStyles(theme => 
+     createStyles({
+ }))
 
 
 export default NavigationBar;
