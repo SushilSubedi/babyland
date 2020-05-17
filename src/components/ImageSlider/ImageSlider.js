1c595c3333a5f3a0bc26070b4399d36986508e2b
bijay subedi <subedibj3030@gmail.com>
2020-05-17 21:34:45 +0530

    carddone

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,59 +1,66 @@
-import React,{Component} from 'react';
-import './ImageSlider.css';
+import React from 'react';
+import { Box, makeStyles, createStyles, Typography, Button } from '@material-ui/core';
 
-class ImageSlider extends Component{
-render(){
+
+
+
+const ImageSlider = () =>{
+    const classes = useStyles();
     return(
-        <div>
-        <div className="box a1">
-        <div className="image_1">
-            <div className="text">
-                <h2>Lorem Ipsum</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, deserunt amet. Similique eos sunt harum earum enim, veritatis distinctio quo qui incidunt laboriosam. Odio fugit possimus, nulla dolorem placeat atque.
-                </p>
-            </div>
-        </div>
-    </div>
+        
+        <Box className={classes.Footer}>
+          
+          
 
-    <div className="box a2">
-        <div className="image_2">
-            <div className="text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam modi, ducimus dignissimos ipsa harum quam nobis officiis odit tempora autem magni! Facilis reiciendis perferendis, quas quos sit dolor modi odit.</p>
-            </div>
-        </div>
-    </div>
-    <div className="box a3">
-        <div className="image_3">
-            <div className="text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non illum nulla consectetur. Sit, unde, debitis ad sunt vitae eligendi tempore, sed expedita aliquid animi enim? Consequatur dignissimos facere recusandae excepturi!</p>
-            </div>
-        </div>
-    </div>
-
-    
-    <div className="box a4">
-        <div className="image_4">
-            <div className="text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic aliquam, unde animi excepturi quos doloribus sed veritatis voluptatum sit quisquam velit corporis, voluptas vitae placeat accusantium! Perferendis sunt molestias est.</p>
-            </div>
-        </div>
-    </div>
-
-    
-    <div className="box a5">
-        <div className="image_5">
-            <div className="text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro tempore vero molestias atque inventore impedit modi repellendus veniam quisquam eum iusto nesciunt sed earum, ut beatae dignissimos magnam accusantium quos?</p>
-            </div>
-        </div>
-    </div>
-    </div>
+          
+        </Box>        
     )
-}
-}
-export default ImageSlider;
+
+ }
+
+ const useStyles = makeStyles(theme => 
+    createStyles({
+        Footer: {
+            padding:'1%',
+            backgroundColor:'pink',
+            display:'flex',
+            justifyContent:'space-between',
+         },
+         copyright:{
+           alignSelf:'flex-end',
+           color:'black',
+         },
+         Icon:{
+           marginLeft:'6px',
+           color:'secondary',
+         },
+         connectus:{
+           alignSelf:'flex-end',
+           marginTop:'3%',
+           color:'blue',
+         },
+         followus:{
+           color:'#000080',
+           fontSize:'20px',
+
+         },
+         contactustext:{
+           color:'#000080',
+           fontSize:'20px',
+         },
+         text:{
+           color:'black',
+           fontSize:'15px',
+         },
+         bloghead:{
+           color:'#000080',
+           fontSize:'20px',
+         },
+         blogtext:{
+           color:'black',
+         }
+
+    }));
+
+
+ export default ImageSlider;
