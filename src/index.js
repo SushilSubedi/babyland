09c4ae8c60bca42be8d6700ff5099cf457429d25
Sushil Subedi <sushilsubedi151@gmail.com>
2020-05-19 11:14:23 -0700

    redux stepup ready

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore,applyMiddleware,compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './rootReducer/rootReducer';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const composeEnhancers = process.env.NODE_ENV==='development'? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :null || compose;
+
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+)
+
+ReactDOM.render(app,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
