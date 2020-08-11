eea12557d9549966c22c52da2f49d8e582a7300f
bijay subedi <subedibj3030@gmail.com>
2020-08-11 17:17:29 +0530

    updatedat

--- src/components/Cart/CartRedux/action.js
+++ src/components/Cart/CartRedux/action.js
@@ -4,15 +4,16 @@ export const CART_START = "CART_START";
 export const CART_SUCCESS = "CART_SUCCESS";
 export const CART_FAIL = "CART_FAIL";
 export const CART_UPDATE = "CART_UPDATE";
+export const CART_DELETE = "CART_DELETE";
 
 
 export const cartStart = () => {
-    return{
+    return {
         type: CART_START
     }
 }
 
-export const cartError =  (error) => {
+export const cartError = (error) => {
     return {
         type: CART_FAIL,
         error: error
@@ -26,53 +27,53 @@ export const cartSuccess = (data) => {
     }
 }
 
-export const fetchCartData = () =>{
+export const fetchCartData = () => {
     return (dispatch) => {
         try {
             dispatch(cartStart());
-        const userId = localStorage.getItem('userID');
-        const cartData = [];
-        fire.database().ref(`/cart/${userId}`).on("value",(snapshort)=>{
-            const data = snapshort.val();
-            //abstract keys from an object
-            // const DataArray = Object.assign([],data);
-            if(snapshort.val() !== null){
-                const keys = Object.keys(data);
-                        // console.log("p",keys.length/ === .length)
-                        for(let i = 0; i < keys.length; i++){
-                            const k = keys[i];
-                            const dataCollection ={
-                             name: data[k].name,
-                             description: data[k].description,
-                             value: data[k].value,
-                             img: data[k].img,
-                             id: data[k].id,
-                             postId: data[k].postId
-                            }
-                            cartData.push(dataCollection);
-                            }
-                        if(keys.length === cartData.length){
-                            dispatch(cartSuccess(cartData));
+            const userId = localStorage.getItem('userID');
+            const cartData = [];
+            fire.database().ref(`/cart/${userId}`).on("value", (snapshort) => {
+                const data = snapshort.val();
+                //abstract keys from an object
+                // const DataArray = Object.assign([],data);
+                if (snapshort.val() !== null) {
+                    const keys = Object.keys(data);
+                    // console.log("p",keys.length/ === .length)
+                    for (let i = 0; i < keys.length; i++) {
+                        const k = keys[i];
+                        const dataCollection = {
+                            name: data[k].name,
+                            description: data[k].description,
+                            value: data[k].value,
+                            img: data[k].img,
+                            id: data[k].id,
+                            postId: data[k].postId
+                        }
+                        cartData.push(dataCollection);
+                    }
+                    if (keys.length === cartData.length) {
+                        dispatch(cartSuccess(cartData));
 
-                        } else {
-                            const k = keys[keys.length - 1]
-                            const dataCollections={
-                               name: data[k].name,
-                               description: data[k].description,
-                                value:data[k].value,
-                                img: data[k].img,
-                                id: data[k].id,
-                                postId: data[k].postId
-                            }
-                            dispatch(updateData(dataCollections));
+                    } else {
+                        const k = keys[keys.length - 1]
+                        const dataCollections = {
+                            name: data[k].name,
+                            description: data[k].description,
+                            value: data[k].value,
+                            img: data[k].img,
+                            id: data[k].id,
+                            postId: data[k].postId
+                        }
+                        dispatch(updateData(dataCollections));
                     }
                 }
-          
-        })
-        }catch(error){
+
+            })
+        } catch (error) {
             dispatch(cartError(error));
         }
-        
+
     }
 }
 
